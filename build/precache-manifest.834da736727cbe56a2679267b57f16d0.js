412369d3daa3f514db853b18243ff01d89d4cfcc
Rakhi Shah <rakhishah@Rakhis-MacBook-Pro.local>
2020-08-29 17:33:30 -0500

    added npm

---
+++ build/precache-manifest.834da736727cbe56a2679267b57f16d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3126b7bc991e0ded0d996ca507c6d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c67928b6444ea2a0c7f2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4368f6f2d6f45cc31267",
+    "url": "/static/js/2.54e9dc7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54e9dc7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67928b6444ea2a0c7f2",
+    "url": "/static/js/main.0c7f0226.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "68a8501467f66042deb722d869ad81f4",
+    "url": "/static/media/resume.68a85014.pdf"
+  }
+]);
